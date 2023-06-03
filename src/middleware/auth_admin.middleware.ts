47a837594b40a626348c9c7e044f11cf656d4115
siraj00000 <siraj00000ahmed@gmail.com>
2023-06-03 16:46:48 +0500

    version.0.1 --server

--- src/middleware/auth_admin.middleware.ts
+++ src/middleware/auth_admin.middleware.ts
@@ -1,11 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
-import User from '../models/user.model.js';
+import User, { IUser } from '../models/user.model.js';
 import { ErrorResponse } from '../utils/error_response.utils.js';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
 
 const authAdmin = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await User.findOne({
-            _id: req.user?._id
+            _id: req.user?.id
         });
         if (user?.role !== '1') return next(new ErrorResponse(400, 'Admin resources are denied'));
 
